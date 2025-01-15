49d034cfcf393979d7455e7969cacd62c7fcb0b4
CaueChieratto <caue.chieratto@icloud.com>
2025-01-15 11:57:27 -0300

    testing2

--- src/components/arrivalsAndExits/Arrivals.js
+++ src/components/arrivalsAndExits/Arrivals.js
@@ -32,6 +32,258 @@ export default function Arrivals() {
           </div>
         </div>
       </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Dani Olmo</div>
+            <div className="infos">
+              Transferencia <span className="number">€55M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Leipzig</div>
+            <div className="infos">8 de ago. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Pau Victor</div>
+            <div className="infos">
+              Transferencia <span className="number">€2.7M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Girona</div>
+            <div className="infos">23 de jul. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Dani Olmo</div>
+            <div className="infos">
+              Transferencia <span className="number">€55M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Leipzig</div>
+            <div className="infos">8 de ago. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Pau Victor</div>
+            <div className="infos">
+              Transferencia <span className="number">€2.7M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Girona</div>
+            <div className="infos">23 de jul. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Dani Olmo</div>
+            <div className="infos">
+              Transferencia <span className="number">€55M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Leipzig</div>
+            <div className="infos">8 de ago. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Pau Victor</div>
+            <div className="infos">
+              Transferencia <span className="number">€2.7M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Girona</div>
+            <div className="infos">23 de jul. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Dani Olmo</div>
+            <div className="infos">
+              Transferencia <span className="number">€55M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Leipzig</div>
+            <div className="infos">8 de ago. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Pau Victor</div>
+            <div className="infos">
+              Transferencia <span className="number">€2.7M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Girona</div>
+            <div className="infos">23 de jul. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Dani Olmo</div>
+            <div className="infos">
+              Transferencia <span className="number">€55M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Leipzig</div>
+            <div className="infos">8 de ago. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Pau Victor</div>
+            <div className="infos">
+              Transferencia <span className="number">€2.7M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Girona</div>
+            <div className="infos">23 de jul. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Dani Olmo</div>
+            <div className="infos">
+              Transferencia <span className="number">€55M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Leipzig</div>
+            <div className="infos">8 de ago. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Pau Victor</div>
+            <div className="infos">
+              Transferencia <span className="number">€2.7M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Girona</div>
+            <div className="infos">23 de jul. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Dani Olmo</div>
+            <div className="infos">
+              Transferencia <span className="number">€55M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Leipzig</div>
+            <div className="infos">8 de ago. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Pau Victor</div>
+            <div className="infos">
+              Transferencia <span className="number">€2.7M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Girona</div>
+            <div className="infos">23 de jul. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Dani Olmo</div>
+            <div className="infos">
+              Transferencia <span className="number">€55M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Leipzig</div>
+            <div className="infos">8 de ago. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Pau Victor</div>
+            <div className="infos">
+              Transferencia <span className="number">€2.7M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Girona</div>
+            <div className="infos">23 de jul. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Dani Olmo</div>
+            <div className="infos">
+              Transferencia <span className="number">€55M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Leipzig</div>
+            <div className="infos">8 de ago. de 2024</div>
+          </div>
+        </div>
+      </div>
+      <div className="containerTransfers">
+        <div className="containerInfos">
+          <div className="infosPlayer">
+            <div className="infosTitles">Pau Victor</div>
+            <div className="infos">
+              Transferencia <span className="number">€2.7M</span>
+            </div>
+          </div>
+          <div className="infosTransfer">
+            <div className="infosTitles">Girona</div>
+            <div className="infos">23 de jul. de 2024</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
